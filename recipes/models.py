2bec0705deda5fa6f491468729a419b1773a580b
theo.fratia <theofratia22@gmail.com>
2025-03-04 15:38:56 +0100

    Add Ingredient model

--- recipes/models.py
+++ recipes/models.py
@@ -13,3 +13,20 @@ class Recette(models.Model):
 
     def __str__(self):
         return self.nom
+
+class Ingredient(models.Model):
+    nom = models.CharField(max_length=255, unique=True)
+    status = models.CharField(max_length=20, choices=[('Actif', 'Actif'), ('Inactif', 'Inactif')], default='Actif')
+
+    def __str__(self):
+        return self.nom
+
+class IngredientRecette(models.Model):
+    recette = models.ForeignKey(Recette, on_delete=models.CASCADE, related_name="ingredients_recette")
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name="recettes_utilisant")
+    quantite = models.FloatField()
+    unite = models.CharField(max_length=50)
+    status = models.CharField(max_length=20, choices=[('Actif', 'Actif'), ('Inactif', 'Inactif')], default='Actif')
+
+    def __str__(self):
+        return f"{self.quantite} {self.unite} de {self.ingredient.nom} pour {self.recette.nom}"
