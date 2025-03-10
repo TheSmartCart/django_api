9d96cdb8053d8ba80e493502ab5a87f212791807
theo.fratia <theofratia22@gmail.com>
2025-03-10 11:16:36 +0100

    add template

--- smartcart/settings.py
+++ smartcart/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'drf_spectacular',
+    'drf_spectacular_sidecar',
     'users',
     'rest_framework',
 ]
