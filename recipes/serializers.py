9dded6213035b6a70663f702175f8f824db563b2
theo.fratia <theofratia22@gmail.com>
2025-03-12 14:00:03 +0100

    Modify get recette

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,21 +1,35 @@
 from rest_framework import serializers
 from .models import (Recette, Ingredient, IngredientRecette, Etape)
 
-class RecetteSerializer(serializers.ModelSerializer):
+class EtapeSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Recette
-        fields = '__all__'
-
+        model = Etape
+        fields = ["id", "ordre", "description"]
 
 class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ingredient
         fields = ["id", "nom"]
 
-class EtapeSerializer(serializers.ModelSerializer):
+class IngredientRecetteDetailSerializer(serializers.ModelSerializer):
+    ingredient = IngredientSerializer(read_only=True)
+    
     class Meta:
-        model = Etape
-        fields = ["id", "ordre", "description"]
+        model = IngredientRecette
+        fields = ["id", "ingredient", "quantite", "unite"]
+
+class RecetteSerializer(serializers.ModelSerializer):
+    ingredients = serializers.SerializerMethodField()
+    etapes = EtapeSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = Recette
+        fields = '__all__'
+    
+    def get_ingredients(self, obj):
+        ingredients_recette = IngredientRecette.objects.filter(recette=obj, status='Actif')
+        return IngredientRecetteDetailSerializer(ingredients_recette, many=True).data
+
 
 class IngredientRecetteSerializer(serializers.ModelSerializer):
     ingredient_nom = serializers.ReadOnlyField(source="ingredient.nom")
