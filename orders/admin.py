51faf8cbb2880da4bcfd47dfc2bd9da108ce6ab2
theo.fratia <theofratia22@gmail.com>
2025-03-14 14:14:06 +0100

    modify admin orders file

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Commande, ArticleCommande
 
-# Register your models here.
+class ArticleCommandeInline(admin.TabularInline):
+    model = ArticleCommande
+    extra = 0
+    readonly_fields = ['prix_total']
+
+@admin.register(Commande)
+class CommandeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'utilisateur', 'magasin', 'statut', 'date_creation', 'prix_total')
+    list_filter = ('statut', 'magasin')
+    search_fields = ('utilisateur__username', 'magasin__nom')
+    inlines = [ArticleCommandeInline]
+    readonly_fields = ['prix_total']
+    fieldsets = (
+        (None, {
+            'fields': ('utilisateur', 'magasin', 'statut')
+        }),
+        ('Informations', {
+            'fields': ('date_creation', 'date_modification', 'prix_total')
+        }),
+    )
+
+@admin.register(ArticleCommande)
+class ArticleCommandeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'commande', 'produit', 'quantite', 'prix_unitaire', 'prix_total')
+    list_filter = ('commande__statut',)
+    search_fields = ('commande__id', 'produit__nom')
