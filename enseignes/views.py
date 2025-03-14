f1840248f7fc8af037865ad5c2b94ee47d400585
theo.fratia <theofratia22@gmail.com>
2025-03-14 12:17:51 +0100

    Update views.py

--- enseignes/views.py
+++ enseignes/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Enseigne, Produit, Categorie
+from .serializers import EnseigneSerializer, ProduitSerializer, CategorieSerializer
 
-# Create your views here.
+class EnseigneViewSet(viewsets.ModelViewSet):
+    queryset = Enseigne.objects.all()
+    serializer_class = EnseigneSerializer
+
+class ProduitViewSet(viewsets.ModelViewSet):
+    queryset = Produit.objects.all()
+    serializer_class = ProduitSerializer
+    filterset_fields = ['enseigne', 'categorie']
+
+class CategorieViewSet(viewsets.ModelViewSet):
+    queryset = Categorie.objects.all()
+    serializer_class = CategorieSerializer
+    filterset_fields = ['enseigne']
