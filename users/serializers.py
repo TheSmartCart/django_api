2a5461130ce971a3442badb2d70aa253c72bce81
TheoFratia <theofratia22@gmail.com>
2026-05-18 12:07:31 +0200

    feat: return token and refresh token when create account

--- users/serializers.py
+++ users/serializers.py
@@ -4,12 +4,27 @@ from .models import CustomUser
 
 class CustomUserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True)
+    access = serializers.SerializerMethodField(required=False)
+    refresh = serializers.SerializerMethodField(required=False)
 
     class Meta:
         model = CustomUser
-        fields = ['id', 'username', 'email', 'image_profil', 'first_name', 'last_name', 'password']
+        fields = ['id', 'username', 'email', 'image_profil', 'first_name', 'last_name', 'password', 'access', 'refresh']
+
+    def get_access(self, obj):
+        return getattr(obj, 'access', None)
+
+    def get_refresh(self, obj):
+        return getattr(obj, 'refresh', None)
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        if not data.get('access'):
+            data.pop('access', None)
+        if not data.get('refresh'):
+            data.pop('refresh', None)
+        return data
 
     def create(self, validated_data):
-        """Créer un utilisateur avec un mot de passe hashé"""
         validated_data['password'] = make_password(validated_data['password'])
         return super().create(validated_data)
--- users/views.py
+++ users/views.py
@@ -1,13 +1,27 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from .models import CustomUser
 from .serializers import CustomUserSerializer
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
 
 class CustomUserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        
+        refresh = RefreshToken.for_user(user)
+        user.refresh = str(refresh)
+        user.access = str(refresh.access_token)
+        
+        response_serializer = self.get_serializer(user)
+        headers = self.get_success_headers(response_serializer.data)
+        return Response(response_serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
     @action(detail=False, methods=['get', 'put', 'patch'], permission_classes=[IsAuthenticated], url_path='me')
     def me(self, request):
@@ -22,7 +36,6 @@ class CustomUserViewSet(viewsets.ModelViewSet):
         return Response(serializer.data)
 
     def get_permissions(self):
-        """Autorise tout le monde à s'inscrire, mais protège les autres actions."""
         if self.action in ['create', 'token', 'token_refresh']:
             return [AllowAny()]
         return [IsAuthenticated()] 
